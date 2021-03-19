c008fe111f18c5a6845a838060050103aed32e05
Norbytus <norbyt93@gmail.com>
2021-03-19 10:10:21 +0300

    Move add validator interface, update psalm

--- src/DependencyInjection/ApiResponseExtension.php
+++ src/DependencyInjection/ApiResponseExtension.php
@@ -4,13 +4,33 @@ declare(strict_types=1);
 
 namespace Homeapp\ExchangeBundle\DependencyInjection;
 
+use Exception;
+use InvalidArgumentException;
+use Symfony\Component\Config\Exception\FileLocatorFileNotFoundException;
+use Symfony\Component\Config\Exception\LoaderLoadException;
+use LogicException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException as ExceptionInvalidArgumentException;
+use Symfony\Component\DependencyInjection\Exception\BadMethodCallException;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use TypeError;
 
 class ApiResponseExtension extends Extension
 {
+    /**
+     * @throws InvalidArgumentException
+     * @throws FileLocatorFileNotFoundException
+     * @throws ExceptionInvalidArgumentException
+     * @throws TypeError
+     * @throws LoaderLoadException
+     * @throws BadMethodCallException
+     * @throws ServiceNotFoundException
+     * @throws LogicException
+     * @throws Exception
+     */
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader(
--- src/HTTP/DtoArgumentValueResolver.php
+++ src/HTTP/DtoArgumentValueResolver.php
@@ -5,32 +5,70 @@ declare(strict_types=1);
 namespace Homeapp\ExchangeBundle\HTTP;
 
 use JMS\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\ConstraintViolation;
 
 class DtoArgumentValueResolver implements ArgumentValueResolverInterface
 {
     private $serializer;
 
-    public function __construct(SerializerInterface $serializer)
-    {
+    private ValidatorInterface $validator;
+
+
+    public function __construct(
+        SerializerInterface $serializer,
+        ValidatorInterface $validator
+    ) {
         $this->serializer = $serializer;
+        $this->validator = $validator;
     }
 
     public function supports(Request $request, ArgumentMetadata $argument)
     {
-        return stripos((string) $argument->getType(), 'App\\DTO') === 0;
+        $typeInUppercase = strtoupper((string)$argument->getType());
+        return false !== strpos($typeInUppercase, 'DTO');
     }
 
+    /**
+     * @throws BadRequestException
+     */
     public function resolve(Request $request, ArgumentMetadata $argument)
     {
 
         /** @psalm-suppress ArgumentTypeCoercion */
-        yield $this->serializer->deserialize(
-            (string) $request->getContent(),
-            (string) $argument->getType(),
+        $dto = $this->serializer->deserialize(
+            (string)$request->getContent(),
+            (string)$argument->getType(),
             'json'
         );
+
+        $validationResult = $this->validator->validate($dto);
+        $this->printErrors($validationResult);
+
+        yield $dto;
+    }
+
+    /**
+     * @throws BadRequestException
+     */
+    private function printErrors(ConstraintViolationListInterface $errors): void
+    {
+        if (count($errors) > 0) {
+            $text = '';
+            /** @var ConstraintViolation $error */
+            foreach ($errors as $error) {
+                $text .= sprintf(
+                    "Ошибка в поле %s: %s\n",
+                    $error->getPropertyPath(),
+                    (string)$error->getMessage()
+                );
+            }
+            throw new BadRequestException($text);
+        }
     }
 }
